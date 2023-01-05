c950f07002b98e32d96b3df1a8f45c55d8fad430
Paul Trott <paulhtrott@gmail.com>
2023-01-05 13:10:43 -0600

    Updated the version, updated workflow to zip a directory, added release to git ignore

--- pressable-onepress-login.php
+++ pressable-onepress-login.php
@@ -10,7 +10,7 @@ Plugin Name: Pressable OnePress Login
 Plugin URI: https://my.pressable.com
 Description: Pressable OnePress Login for the MyPressable Control Panel.
 Author: Pressable
-Version: 1.1.0
+Version: 1.2.0
 Author URI: https://my.pressable.com/
 License: GPL2
 */
